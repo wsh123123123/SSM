1ca63ece52f6093f2015686dc26c8a08d1d90f63
wsh <3070014157@qq.com>
2022-12-26 19:49:34 +0800

    first commit

---
+++ git_test/src/main/java/com/atguigu/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu;
+
+/**
+ * @author wsh
+ * @create 2022-12-26 19:47
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+    }
+}
